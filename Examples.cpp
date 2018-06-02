6c1d834f0567357ae783234f87e3264a23465b37
Leonardo Machado <leonardoms@id.uff.br>
2018-06-01 21:52:14 -0300

    Schema validation added. Number allowed and disallowed options added.

--- Examples.cpp
+++ Examples.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
-#include <string>
 #include "Joi.hpp"
 
 using namespace std;
 
 int main()
 {
-	string input;
+	struct Model
+	{
+		int id = 10;
+		char name[32] = { "Leonardo" };
+		int age = 18;
+		char email[50] = { "leonardo@gmail.com" };
+	} model;
 
-	cout << "Digite seu nome: ";
-	cin >> input;
+	struct Schema
+	{
+		JoiNumber id = JoiNumber::build().positive().less(100);
+		JoiString name = JoiString::build().pattern("[A-Z][a-z]+").maximum(31).required();
+		JoiNumber age = JoiNumber::build().minimum(18).maximum(65);
+		JoiString email = JoiString::build().pattern("^\\S+@\\S+$").maximum(49);
+		JoiString test = JoiString::build().alphanum().minimum(49).required();
+	} schema;
 
-	JoiString schema = JoiString::build().alphanum().maximum(32).required();
-	JoiResult result = JoiString::validate(input, &schema);
 
-	if(result.error())
-		cout << result.description() << endl;
+	JoiResult validation = Joi::validate(Joi::key(model.id, schema.id),
+										 Joi::key(model.name, schema.name),
+										 Joi::key(model.age, schema.age));
+
+	if(validation.error())
+		cout << validation.description() << endl;
 	else
-		cout << "Nome valido ;)" << endl;
+		cout << "All good ~;)" << endl;
 
-    return 0;
+	return 0;
 }
--- Joi.hpp
+++ Joi.hpp
@@ -1,43 +1,15 @@
-/**
-*
-*  Input Validation Module
-*  Author: Leonardo Machado [05/31/2018]
-*
-*  Description:
-*   A header-only runtime input validator inspired in Joi module for javascript [ https://github.com/hapijs/joi ]
-*
-*	Input Validation:
-*
-* 		Example:
-*
-* 		int main()
-*		{
-*			int value = 10;
-*
-*			JoiNumber schema = JoiNumber::build().less(1000).positive();
-*
-*			JoiResult result = JoiNumber::validate(value, &schema);
-*
-*			if(result.error())
-*				cout << result.description() << endl;
-*			else
-*				cout << "parsed successfully ;)" << endl;
-*
-*			return 0;
-*		}
-*
-
-*/
-
 #ifndef JOI_H
 #define JOI_H
 
-#include <string>
+// Includes
+#include <vector>
 #include <regex>
+#include <string>
+#include <tuple>
+#include <ctype.h>
 
-// Builder Design Pattern
-using namespace std;
 
+// Result of validations
 class JoiResult
 {
 private:
@@ -45,127 +17,208 @@ private:
 	std::string _description;
 
 public:
+	// Constructor
 	JoiResult()
 	{
 		error(false);
 		description("");
 	}
-
 	JoiResult(bool err, const std::string &desc)
 	{
 		error(err);
 		description(desc);
 	}
 
-	~JoiResult() {}
+	// Destructor
+	virtual ~JoiResult() {}
 
+	// Setter
 	void error(bool value)
 	{
 		_error = value;
 	}
-
 	void description(const std::string &desc)
 	{
 		_description = desc;
 	}
 
+	// Getter
 	bool error() const
 	{
 		return _error;
 	}
-
-	string description() const
+	std::string description() const
 	{
 		return _description;
 	}
-
 };
 
+// Validate Integer Numbers
 class JoiNumber
 {
 private:
-	int _min; // Specifies the minimum value where _min is the minimum value allowed.
-	int _max; // Specifies the maximum value where _max is the maximum value allowed.
-	int _less; // Specifies that the value must be less than _less.
-	int _greater; // Specifies that the value must be greater than _greater.
-	bool _negative; // Requires the number to be negative.
-	bool _positive; // Requires the number to be positive.
+	// C++ 11 struct initialization 	
+	struct Number
+	{
+		bool flag = false;
+		int value = 0;
+	};
+
+	// C++ 11 struct initialization 	
+	struct NumberList
+	{
+		bool flag = false;
+		std::vector<int> values;
+	};
+
+	Number _min; // Specifies the minimum value where _min.value is the minimum value allowed.
+	Number _max; // Specifies the maximum value where _max is the maximum value allowed.
+	NumberList _disallow; // Specifies the invalids input.
+	NumberList _allow; // Specifies the valids input.
+	Number _less; // Specifies that the value must be less than _less.
+	Number _greater; // Specifies that the value must be greater than _greater.
+	Number _negative; // Requires the number to be negative.
+	Number _positive; // Requires the number to be positive.
 
 private:
-	JoiNumber()
-	{
-		_min = INT_MAX; // Specifies the minimum value where _min is the minimum value allowed.
-		_max = INT_MIN; // Specifies the maximum value where _max is the maximum value allowed.
-		_less = INT_MIN; // Specifies that the value must be less than _less.
-		_greater = INT_MAX; // Specifies that the value must be greater than _greater.
-		_negative = false; // Requires the number to be negative.
-		_positive = false; // Requires the number to be positive.
-	}
+	// Private Constructor to prevent instance creation
+	// JoiNumber must be created with JoiNumber::build() method.
+	JoiNumber() {}
 
 public:
-
+	// Destructor
 	virtual ~JoiNumber() {}
 
-	JoiNumber minimum(int value)
+	// Setters
+	JoiNumber minimum(const int &value)
 	{
-		_min = value;
+		_min.flag = true;
+		_min.value = value;
+
 		return *this;
 	}
-	JoiNumber maximum(int value)
+	JoiNumber maximum(const int &value)
 	{
-		_max = value;
+		_max.flag = true;
+		_max.value = value;
 		return *this;
 	}
-	JoiNumber less(int value)
+	template<typename ...Ints>
+	JoiNumber disallow(Ints... ints)
 	{
-		_less = value;
+		_disallow.flag = true;
+
+		int args[]{ ints... };
+
+		for(auto &v : args)
+			_disallow.values.push_back(v);
+
 		return *this;
 	}
-	JoiNumber greater(int value)
+	template<typename ...Ints>
+	JoiNumber allow(Ints... ints)
 	{
-		_greater = value;
+		_allow.flag = true;
+
+		int args[]{ ints... };
+
+		for(auto &v : args)
+			_allow.values.push_back(v);
+
+		return *this;
+	}
+	JoiNumber less(const int &value)
+	{
+		_less.flag = true;
+		_less.value = value;
+		return *this;
+	}
+	JoiNumber greater(const int &value)
+	{
+		_greater.flag = true;
+		_greater.value = value;
 		return *this;
 	}
 	JoiNumber negative()
 	{
-		_negative = true;
+		_negative.flag = true;
 		return *this;
 	}
 	JoiNumber positive()
 	{
-		_positive = true;
+		_positive.flag = true;
 		return *this;
 	}
 
+	// Builder Design Pattern -  Instance Creator
 	static JoiNumber build()
 	{
 		return JoiNumber();
 	}
-	static JoiResult validate(const int value, JoiNumber *schema)
+
+	JoiResult validate(const int &value) const
 	{
 		// Validate _min
-		if(schema->_min != INT_MAX && value < schema->_min)
-			return JoiResult(true, "The number value must be at least " + std::to_string(schema->_min) + ". Value: [ " + std::to_string(value) + " ]");
+		if(_min.flag)
+		{
+			if(value < _min.value)
+			{
+				return JoiResult(true, "The number must be at least " + std::to_string(_min.value) + ". Number: [ " + std::to_string(value) + " ]");
+			}
+		}
 
 		// Validate _max
-		if(schema->_max != INT_MIN && value > schema->_max)
-			return JoiResult(true, "The number value must be at most " + std::to_string(schema->_max) + ". Value: [ " + std::to_string(value) + " ]");
+		if(_max.flag)
+		{
+			if(value > _max.value)
+			{
+				return JoiResult(true, "The number must be at most " + std::to_string(_max.value) + ". Number: [ " + std::to_string(value) + " ]");
+			}
+		}
+
+		// Validate _disallow
+		if(_disallow.flag)
+		{
+			if(std::find(_disallow.values.begin(), _disallow.values.end(), value) != _disallow.values.end())
+				return JoiResult(true, "The number [" + std::to_string(value) + "] is in the blacklist.");
+		}
+
+		// Validate _allow
+		if(_allow.flag)
+		{
+			if(std::find(_allow.values.begin(), _allow.values.end(), value) == _allow.values.end())
+				return JoiResult(true, "The number [" + std::to_string(value) + "] isn't in the whitelist.");
+		}
 
 		// Validate _less
-		if(schema->_less != INT_MIN && value >= schema->_less)
-			return JoiResult(true, "The number value must lesser than " + std::to_string(schema->_less) + ". Value: [ " + std::to_string(value) + " ]");
+		if(_less.flag)
+		{
+			if(value >= _less.value)
+			{
+				return JoiResult(true, "The number must be less than " + std::to_string(_less.value) + ". Value: [ " + std::to_string(value) + " ]");
+			}
+		}
 
 		// Validate _greater
-		if(schema->_greater != INT_MAX && value <= schema->_greater)
-			return JoiResult(true, "The number value must greater than " + std::to_string(schema->_greater) + ". Value: [ " + std::to_string(value) + " ]");
+		if(_greater.flag)
+		{
+			if(value <= _greater.value)
+			{
+				return JoiResult(true, "The number must be greater than " + std::to_string(_greater.value) + ". Value: [ " + std::to_string(value) + " ]");
+			}
+		}
 
 		// Validate _negative
-		if(schema->_negative && value >= 0)
-			return JoiResult(true, "The number value must be negative. Value: [ " + std::to_string(value) + " ]");
+		if(_negative.flag && value >= 0)
+		{
+			return JoiResult(true, "The number must be negative. Value: [ " + std::to_string(value) + " ]");
+		}
 
 		// Validate _positive
-		if(schema->_positive && value < 0)
-			return JoiResult(true, "The number value must be positive. Value: [ " + std::to_string(value) + " ]");
+		if(_positive.flag && value < 0)
+		{
+			return JoiResult(true, "The number must be positive. Value: [ " + std::to_string(value) + " ]");
+		}
 
 		// The number value is valid. 
 		// Return NO ERROR flag.
@@ -173,22 +226,25 @@ public:
 	}
 };
 
+// Validate Strings
 class JoiString
 {
 private:
-	bool _required; // Requires the string value to be non-empty.
-	int _min; // Requires the string value to be at least _min characters long.
-	int _max; // Requires the string value to be at most _max characters long.
-	int _length; // Requires the string value to be at exactly _length characters long.
-	string _regex; // Requires the string value to satisfies the regex expression.
-	bool _alpha; // Requires the string value to only contain a-z, A-Z.
-	bool _num; // Requires the string value to only contain 0-9.
-	bool _alphanum; // Requires the string value to only contain a-z, A-Z, and 0-9.
-	bool _ip; // Requires the string value to be a valid ip address v4.
-	bool _lowercase; // Requires the string value to be all lowercase. 
-	bool _uppercase; // Requires the string value to be all uppercase.
+	bool _required; // Requires the string to be non-empty.
+	int _min; // Requires the string to be at least _min characters long. >=
+	int _max; // Requires the string to be at most _max characters long. <=
+	int _length; // Requires the string to be at exactly _length characters long. ==
+	std::string _regex; // Requires the string to satisfies the regex expression.
+	bool _alpha; // Requires the string to only contain a-z, A-Z.
+	bool _num; // Requires the string to only contain 0-9.
+	bool _alphanum; // Requires the string to only contain a-z, A-Z, and 0-9.
+	bool _ip; // Requires the string to be a valid ip address v4.
+	bool _lowercase; // Requires the string to be all lowercase. 
+	bool _uppercase; // Requires the string to be all uppercase.
 
 private:
+	// Private Constructor to prevent instance creation
+	// JoiString must be created with JoiString::build() method.
 	JoiString()
 	{
 		_required = false;
@@ -205,9 +261,11 @@ private:
 	}
 
 public:
+	// Destructor
 	virtual ~JoiString()
 	{}
 
+	// Setters
 	JoiString required()
 	{
 		_required = true;
@@ -264,114 +322,139 @@ public:
 		return *this;
 	}
 
-	// Builder Design Pattern
+	// Builder Design Pattern -  Instance Creator
 	static JoiString build()
 	{
 		return JoiString();
 	}
 
-	static JoiResult validate(const string &value, JoiString *schema)
+	JoiResult validate(const std::string &value) const
 	{
 		// Validate _required
-		if(schema->_required && schema->length(value, 1) == 0)
-			return JoiResult(true, "The string value is required");
-
+		if(_required && length(value, 1) == 0)
+		{
+			return JoiResult(true, "The string is required");
+		}
 
 		// Validate _min
-		if(schema->_min >= 0)
+		if(_min >= 0)
 		{
-			int len = schema->length(value, schema->_min);
+			int len = length(value, _min);
 
-			if(len < schema->_min)
-				return JoiResult(true, "The string value must be at least " + std::to_string(schema->_min) + " characters long");
+			if(len < _min)
+			{
+				if(_required)
+					return JoiResult(true, "The string must be at least " + std::to_string(_min) + " characters long");
+			}
 		}
 
 
 		// Validate _max
-		if(schema->_max > 0)
+		if(_max > 0)
 		{
-			int len = schema->length(value, schema->_max + 1);
+			int len = length(value, _max + 1);
 
-			if(len > schema->_max)
-				return JoiResult(true, "The string value must be at most " + std::to_string(schema->_max) + " characters long");
+			if(len > _max)
+			{
+				return JoiResult(true, "The string value be at most " + std::to_string(_max) + " characters long");
+			}
 		}
 
 
 		// Validate _length
-		if(schema->_length > 0)
+		if(_length > 0)
 		{
-			int len = schema->length(value, schema->_length + 1);
+			int len = length(value, _length + 1);
 
-			if(len != schema->_length)
-				return JoiResult(true, "The string value must be " + std::to_string(schema->_length) + " characters long");
+			if(len != _length)
+			{
+				if(_required)
+					return JoiResult(true, "The string value must be " + std::to_string(_length) + " characters long");
+			}
 		}
 
 
 		// Validate _regex
-		if(schema->_regex.size())
+		if(_regex.size())
 		{
-			std::regex validation(schema->_regex);
+			std::regex validation(_regex);
 
 			if(!std::regex_match(value, validation))
-				return JoiResult(true, "Regex validation failed");
+			{
+				if(_required)
+					return JoiResult(true, "Regex validation failed");
+			}
 		}
 
 		// Validate _alpha
-		if(schema->_alpha)
+		if(_alpha)
 		{
 			for(auto &c : value)
 			{
 				if(!isalpha(c))
+				{
 					return JoiResult(true, "The string value can only contain a-z, A-Z.");
+				}
 			}
 		}
 
 		// Validate _num
-		if(schema->_num)
+		if(_num)
 		{
 			for(auto &c : value)
 			{
 				if(!isdigit(c))
+				{
 					return JoiResult(true, "The string value can only contain 0-9.");
+				}
 			}
 		}
 
 		// Validate _alphanum
-		if(schema->_alphanum)
+		if(_alphanum)
 		{
 			for(auto &c : value)
 			{
 				if(!isalnum(c))
+				{
 					return JoiResult(true, "The string value can only contain a-z, A-Z, and 0-9.");
+				}
 			}
 		}
 
 		// Validate _ip
-		if(schema->_ip)
+		if(_ip)
 		{
 			std::regex validation("^(?=\\d+\\.\\d+\\.\\d+\\.\\d+$)(?:(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\\.?){4}$"); // IP v4 VALIDATION
 
 			if(!std::regex_match(value, validation))
-				return JoiResult(true, "IP validation failed");
+			{
+				if(_required)
+					return JoiResult(true, "IP validation failed");
+			}
 		}
 
 		// Validate _lowercase
-		if(schema->_lowercase)
+		if(_lowercase)
 		{
 			for(auto &c : value)
 			{
 				if(isalpha(c) && !islower(c))
+				{
 					return JoiResult(true, "The string value can only contain lowercase characters.");
+				}
 			}
 		}
 
 		// Validate _uppercase
-		if(schema->_uppercase)
+		if(_uppercase)
 		{
 			for(auto &c : value)
 			{
 				if(isalpha(c) && !isupper(c))
+				{
 					return JoiResult(true, "The string value can only contain uppercase characters.");
+				}
 			}
 		}
 
@@ -382,17 +465,46 @@ public:
 	}
 
 private:
-	int length(const string& value, int max) const
+	// Helper functions
+	int length(const std::string& value, int max) const
 	{
 		int len = 0;
 
 		while(value[len] > 0 && len < max)
+		{
 			len++;
+		}
 
 		return len;
 	}
-
 };
 
+// Wrapper Class
+class Joi
+{
+public:
+	template <typename T>
+	static JoiResult validate(T param)
+	{
+		return std::get<1>(param).validate(std::get<0>(param));
+	}
+
+	template <typename T, typename... Params>
+	static JoiResult validate(T param, Params... params)
+	{
+		JoiResult result = Joi::validate(param);
+
+		if(result.error())
+			return result;
+
+		return Joi::validate(params...);
+	}
+
+	template <typename T1, typename T2>
+	static std::tuple<T1, T2> key(T1 arg1, T2 arg2)
+	{
+		return std::make_tuple(arg1, arg2);
+	}
+};
 
 #endif // JOI_H
